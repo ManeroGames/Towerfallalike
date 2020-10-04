42671fa94923b54cb2c0d74c18af4346a9133d39
Rafael Machado <33268419+machadorafa@users.noreply.github.com>
2020-10-04 20:27:57 -0300

    gitignore fixes

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public int health = 100;
+
+	public GameObject deathEffect;
+	private GameObject clone;
+
+	public void TakeDamage (int damage)
+	{
+		health -= damage;
+
+		if (health <= 0)
+		{
+			Die();
+		}
+	}
+
+	void Die ()
+	{
+		clone = (GameObject)Instantiate(deathEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+		Destroy(clone, 0.5f);
+	}
+
+}
