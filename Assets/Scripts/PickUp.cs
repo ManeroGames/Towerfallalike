af5b190c98cb184e6cd5b2a72cf7710c6cfc3272
Gabriel Marquesi <gabriel.mmarquesi@gmail.com>
2020-10-05 05:54:04 -0300

    Inventory System / items Pickup System

---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    private Inventory inventory;
+    public GameObject itemButton;
+
+    private void Start() 
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.CompareTag("Player"))
+        {
+            for (int i = 0; i < inventory.slots.Length; i++)
+            {
+                if(inventory.isFull[i] == false)
+                {
+                    inventory.isFull[i] = true;
+                    Instantiate(itemButton, inventory.slots[i].transform, false); //instancia o 'item' Button no mesmo local do slot
+                    Destroy(gameObject);
+                    break;
+                }
+            }       
+        }
+                    
+    }
+}
---
+++ Assets/Scripts/PotionItem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotionItem : MonoBehaviour
+{
+    public GameObject effect;
+    private Transform player;
+
+    private void Start() {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    public void Use ()
+    {
+        Instantiate(effect, player.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
